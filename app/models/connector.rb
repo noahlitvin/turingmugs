940c00fa5fd0c24f57f400ff19b76828c543ff9f
noah <noah@noahlitvin.com>
2015-10-22 16:42:08 -0400

    progress

--- app/models/connector.rb
+++ app/models/connector.rb
@@ -7,6 +7,6 @@ class Connector < ActiveRecord::Base
 
 	def announce_user_number_change
     	client = Slack::Web::Client.new
-    	client.message text: "I now send messages in this channel to " + self.user_number, channel: self.channel if self.user_number_changed?
+    	client.chat_postMessage text: "I now send messages in this channel to " + self.user_number, channel: self.channel, as_user: true if self.user_number_changed?
 	end
 end

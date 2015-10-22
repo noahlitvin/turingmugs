d70408c7e1db03169c71316f7288112e4848497b
noah <noah@noahlitvin.com>
2015-10-22 15:56:45 -0400

    adds callback

--- app/models/connector.rb
+++ app/models/connector.rb
@@ -5,7 +5,7 @@ class Connector < ActiveRecord::Base
 
 	after_save :announce_user_number_change
 
-	def announce_user_number_change
+	def announce_user_number_change(connector)
     	client = Slack::Web::Client.new
     	client.message text: "I now send messages in this channel to " + connector.user_number, channel: connector.channel if connector.user_number_changed?
 	end

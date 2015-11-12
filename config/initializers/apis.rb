93bef0da648eb161758db65ae97ec7f3135ab810
noah <noah@noahlitvin.com>
2015-11-12 12:30:27 -0500

    direct send command

--- config/initializers/apis.rb
+++ config/initializers/apis.rb
@@ -12,13 +12,22 @@ module MugBot
 
   class Sender < SlackRubyBot::Commands::Base
     match /^*/ do |client, data, match|
-      #Log.create(title: "Outbound message received.")
       if data.user != client.self['id'] and data['subtype'].blank?
         connector = Connector.find_by(channel: data.channel)
         if !connector
           client.message text: "I don't have a connector for the channel, so I didn't send this message along.", channel: data.channel
         elsif connector.user_number.blank?
           client.message text: "I don't have a user number associated with this channel, so I didn't send this message along.", channel: data.channel
+        elsif data.text and data.text.start_with?('-send')
+          target_number = data.text.split(" ")[1]
+          target_number = "+1" + target_number if target_number.length == 10
+          text = data.text.split(" ").drop(2).join(" ")
+          message = TwilioClient.messages.create(
+            from: connector.mug_number,
+            to: target_number,
+            body: text
+          )
+          Log.create(title: "Text message sent.", raw_data: {to: target_number,from: connector.mug_number,body: text})
         else
           message = TwilioClient.messages.create(
             from: connector.mug_number,
